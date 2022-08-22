0051b316e955fd7d197e416be23701ff50b991ec
latenightluke <luke.ivie@icloud.com>
2022-08-21 23:42:30 -0700

    feat: store session in db and user info in auth0

---
+++ prisma/migrations/20220821233451_remove_user_details/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `email` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `firstName` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `lastName` on the `User` table. All the data in the column will be lost.
+  - Added the required column `authZeroId` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX "User_email_key";
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "email",
+DROP COLUMN "firstName",
+DROP COLUMN "lastName",
+ADD COLUMN     "authZeroId" TEXT NOT NULL;
